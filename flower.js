00a3913ccbf5c6bac4479994a828c4429b57d3bf
Horizon-dev4 <horizonsolutions.dev4@gmail.com>
2022-12-18 11:59:21 +0600

    new line added in file.js

--- flower.js
+++ flower.js
@@ -3,5 +3,6 @@ let rose = 'We have a huge rose garden in our backyard.'
 console.log(rose);
 if(rose.length > 13){
     let maxLine = rose.slice(0, 14);
-    console.log(maxLine + '\n' +'working');
+    let stuName = " rayhan bhuiyaa";
+    console.log(maxLine + '\n' +'working' + stuName);
 }

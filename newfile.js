4a012593cf0c7d67fba15bebf4bf249bfc53a200
Horizon-dev4 <horizonsolutions.dev4@gmail.com>
2022-12-18 12:56:34 +0600

    forEach() loop implemented in newfile.js

--- newfile.js
+++ newfile.js
@@ -1,2 +1,11 @@
 let s = ["apple","orange","lichi"];
 console.log(s);
+
+let animals = ["dog", "cow", "horse", "monkey"];
+animals.forEach(animal =>{
+    //console.log(animal);
+    let persons = ["akib", "nakib", "sakib", "rakibh"]
+    persons.forEach(heGets =>{
+        console.log(heGets + ' gets ' + animal + '\n');
+    })
+});

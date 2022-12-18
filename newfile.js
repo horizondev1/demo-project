65ffa838fb36025e0377ab8a4271a6dc50869aa1
horizondev1 <horizonsolutions.dev1@gmail.com>
2022-12-18 12:56:09 +0600

    newfile has been upadted

--- newfile.js
+++ newfile.js
@@ -1,2 +1,7 @@
-let s = ["apple","orange","lichi"];
-console.log(s);
+let sports = ["arg","bra","fra","ita","zer","por","cro","moro","aus","arg","arg"];
+let i = 0;
+for(; i<sports.length;i++){
+    if (sports[i]== "arg"){
+       console.log(sports[i]);
+    }
+}
